a070dfb96d67b04b8bcdb330511ed1cc44f107c8
pavithra <pavithra.25cs@licet.ac.in>
2023-07-25 21:28:52 +0530

    Main Program

---
+++ run.py
@@ -0,0 +1,17 @@
+from  flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+app=Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///database.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
+
+db=SQLAlchemy(app)
+m=Marshmallow(app)
+
+
+if __name__=="__main__":
+    app.run(debug=True) 
+
+import routes
